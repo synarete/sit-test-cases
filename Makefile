16c8744ebaf06fcc789173930ef3d5c1790eb8bf
Sachin Prabhu <sprabhu@redhat.com>
2023-02-13 22:23:14 +0000

    tests: Add sanity test makefile target
    For use in the CI environment.
    
    Signed-off-by: Sachin Prabhu <sprabhu@redhat.com>

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 test:
 	@ PYTHONPATH=`pwd` ./run_all_tests.sh test-info.yml
+
+sanity_test:
+	@ PYTHONPATH=`pwd` ./run_all_tests.sh test-info.yml sanity_tests
--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+TESTS_FILE="tests"
+if [ -n "$2" ]
+then
+	TESTS_FILE=$2
+fi
+
 echo_success() {
 	echo  "[ OK ] "
 	return 0
@@ -9,7 +15,7 @@ test_info_file_dir=$(cd `dirname $1` && pwd)
 test_info_file=$test_info_file_dir/`basename $1`
 
 cd testcases
-for i in `cat tests|sed 's/#.*//'|sed 's/  +//g'|grep -v '^ *$'`
+for i in `cat ${TESTS_FILE}|sed 's/#.*//'|sed 's/  +//g'|grep -v '^ *$'`
 do
 	echo -n "$i "
 	if [ -x $i/run_test ] && ! $i/run_test $test_info_file
