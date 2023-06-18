f85c6fbbe9ad9e1ff9b2d201e0d301387e93c8a4
Sachin Prabhu <sprabhu@redhat.com>
2023-06-18 18:47:13 +0100

    consistency_test: clean up in preparation for pytest
    Signed-off-by: Sachin Prabhu <sprabhu@redhat.com>

--- testcases/consistency-test/consistency-check.py
+++ testcases/consistency-test/consistency-check.py
@@ -15,63 +15,70 @@ def file_content_check(f, comp_str):
     read_data = f.read()
     return read_data == comp_str
 
-if (len(sys.argv) != 2):
-    print("Usage: %s <test-info.yml>" % (sys.argv[0]))
-    exit(1)
+def consistency_test(test_info):
+	tmp_root = testhelper.get_tmp_root()
+	mount_point = testhelper.get_tmp_mount_point(tmp_root)
+	mount_params = testhelper.get_default_mount_params(test_info)
 
-test_info_file = sys.argv[1]
-test_info = testhelper.read_yaml(test_info_file)
+	# First open the default share and write a file with contents
+	# set to the test_string
+	flag_share_mounted = 0
+	flag_file_created = 0
+	try:
+	    print("\n")
+	    for sharenum in range(testhelper.get_num_shares(test_info)):
+	        share = testhelper.get_share(test_info, sharenum)
+	        mount_params["share"] = share
+	        print("Testing %s" % (share))
+	        testhelper.cifs_mount(mount_params, mount_point)
+	        flag_share_mounted = 1
+	        test_file = testhelper.get_tmp_file(mount_point)
+	        flag_file_created = 1
+	        with open(test_file, 'w') as f:
+	            f.write(test_string)
+	        testhelper.cifs_umount(mount_point)
+	        flag_share_mounted = 0
 
-tmp_root = testhelper.get_tmp_root()
-mount_point = testhelper.get_tmp_mount_point(tmp_root)
-mount_params = testhelper.get_default_mount_params(test_info)
+	        for i in range(1, testhelper.get_total_mount_parameter_combinations(test_info)):
+	            mount_params = testhelper.get_mount_parameter(test_info, share, i)
+	            testhelper.cifs_mount(mount_params, mount_point)
+	            flag_share_mounted = 1
+	            with open(test_file, 'r') as f:
+	                assert file_content_check(f, test_string), "File content does not match"
+	            testhelper.cifs_umount(mount_point)
+	            flag_share_mounted = 0
 
-# First open the default share and write a file with contents
-# set to the test_string
-flag_share_mounted = 0
-flag_file_created = 0
-try:
-    print("\n")
-    for sharenum in range(testhelper.get_num_shares(test_info)):
-        share = testhelper.get_share(test_info, sharenum)
-        mount_params["share"] = share
-        print("Testing %s" % (share))
-        testhelper.cifs_mount(mount_params, mount_point)
-        flag_share_mounted = 1
-        test_file = testhelper.get_tmp_file(mount_point)
-        flag_file_created = 1
-        with open(test_file, 'w') as f:
-            f.write(test_string)
-        testhelper.cifs_umount(mount_point)
-        flag_share_mounted = 0
+	        if (flag_file_created == 1):
+	            testhelper.cifs_mount(mount_params, mount_point)
+	            flag_share_mounted = 1
+	            os.unlink(test_file)
+	            flag_file_created = 0
+	            testhelper.cifs_umount(mount_point)
+	            flag_share_mounted = 0
+	except:
+	    print("Error while executing test")
+	    raise
 
-        for i in range(1, testhelper.get_total_mount_parameter_combinations(test_info)):
-            mount_params = testhelper.get_mount_parameter(test_info, share, i)
-            testhelper.cifs_mount(mount_params, mount_point)
-            flag_share_mounted = 1
-            with open(test_file, 'r') as f:
-                assert file_content_check(f, test_string), "File content does not match"
-            testhelper.cifs_umount(mount_point)
-            flag_share_mounted = 0
+	finally:
+	    if (flag_file_created == 1):
+	        testhelper.cifs_mount(mount_params, mount_point)
+	        os.unlink(test_file)
+	        testhelper.cifs_umount(mount_point)
+	    if (flag_share_mounted == 1):
+	        testhelper.cifs_umount(mount_point)
+	    os.rmdir(mount_point)
+	    os.rmdir(tmp_root)
 
-        if (flag_file_created == 1):
-            testhelper.cifs_mount(mount_params, mount_point)
-            flag_share_mounted = 1
-            os.unlink(test_file)
-            flag_file_created = 0
-            testhelper.cifs_umount(mount_point)
-            flag_share_mounted = 0
+def test_consistency():
+    test_info_file = os.getenv('TEST_INFO_FILE')
+    test_info = testhelper.read_yaml(test_info_file)
+    consistency_test(test_info)
 
-except:
-    print("Error while executing test")
-    raise
+if __name__ == "__main__":
+    if (len(sys.argv) != 2):
+        print("Usage: %s <test-info.yml>" % (sys.argv[0]))
+        exit(1)
+    test_info_file = sys.argv[1]
+    test_info = testhelper.read_yaml(test_info_file)
+    consistency_test(test_info)
 
-finally:
-    if (flag_file_created == 1):
-        testhelper.cifs_mount(mount_params, mount_point)
-        os.unlink(test_file)
-        testhelper.cifs_umount(mount_point)
-    if (flag_share_mounted == 1):
-        testhelper.cifs_umount(mount_point)
-    os.rmdir(mount_point)
-    os.rmdir(tmp_root)
